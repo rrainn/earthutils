4d5f7336b28677033993e37c8cdabe7905f5f137
Charlie Fish <fishcharlie@me.com>
2021-07-11 12:33:14 -0600

    Moving logic into different file

--- lib/address/unit/parser.ts
+++ lib/address/unit/parser.ts
@@ -1,34 +1,31 @@
-
-/*
-
-returnObject["addr:unit"] = streetParts[streetParts.length - 1].replace("#", "");
-		if (SecondaryUnit.SecondaryUnitAbbreviations[secondLastStreetPart]) {
-			returnObject["addr:unitname"] = SecondaryUnit.SecondaryUnitAbbreviations[secondLastStreetPart];
-		} else if (SecondaryUnit.SecondaryUnitAbbreviationsInverseCaps[secondLastStreetPart]) {
-			const abbreviation = SecondaryUnit.SecondaryUnitAbbreviationsInverseCaps[secondLastStreetPart];
-			returnObject["addr:unitname"] = SecondaryUnit.SecondaryUnitAbbreviations[abbreviation];
-		} else {
-			returnObject["addr:unitname"] = "";
-		}
-		*/
-
 import * as SecondaryUnit from "../../abbreviations/secondaryUnit";
+
+// This function will return the full secondary unit or false
+function standardizeSecondaryUnit(value: string): string | false {
+	const unitName = value.toUpperCase().replace(".", "");
+	const unitNameFirstCap = unitName.charAt(0).toUpperCase() + unitName.slice(1).toLowerCase();
+
+	if (SecondaryUnit.SecondaryUnitAbbreviations[unitName]) {
+		return SecondaryUnit.SecondaryUnitAbbreviations[unitName];
+	} else if (SecondaryUnit.SecondaryUnitAbbreviationsInverseCaps[unitName]) {
+		const abbreviation = SecondaryUnit.SecondaryUnitAbbreviationsInverseCaps[unitName];
+		return SecondaryUnit.SecondaryUnitAbbreviations[abbreviation];
+	} else if (Object.values(SecondaryUnit.SecondaryUnitAbbreviations).includes(unitNameFirstCap)) {
+		return unitNameFirstCap;
+	} else {
+		return false;
+	}
+}
 
 export default function (unit: string): any {
 	const unitParts = unit.replace(/^#/gmu, "").trim().split(" ");
-	const [originalUnitName, ...unitValue] = unitParts;
-	const unitName = originalUnitName.toUpperCase().replace(".", "");
-	const unitNameFirstCap = unitName.charAt(0).toUpperCase() + unitName.slice(1).toLowerCase();
+	const [unitName, ...unitValue] = unitParts;
 
 	const returnObject = {};
 
-	if (SecondaryUnit.SecondaryUnitAbbreviations[unitName]) {
-		returnObject["addr:unitname"] = SecondaryUnit.SecondaryUnitAbbreviations[unitName];
-	} else if (SecondaryUnit.SecondaryUnitAbbreviationsInverseCaps[unitName]) {
-		const abbreviation = SecondaryUnit.SecondaryUnitAbbreviationsInverseCaps[unitName];
-		returnObject["addr:unitname"] = SecondaryUnit.SecondaryUnitAbbreviations[abbreviation];
-	} else if (Object.values(SecondaryUnit.SecondaryUnitAbbreviations).includes(unitNameFirstCap)) {
-		returnObject["addr:unitname"] = unitNameFirstCap;
+	const standardizedSecondaryUnit = standardizeSecondaryUnit(unitName);
+	if (standardizedSecondaryUnit) {
+		returnObject["addr:unitname"] = standardizedSecondaryUnit;
 	} else {
 		returnObject["addr:unitname"] = "";
 	}
@@ -36,7 +33,7 @@ export default function (unit: string): any {
 	returnObject["addr:unit"] = unitValue.join(" ");
 
 	if (returnObject["addr:unitname"] === "") {
-		returnObject["addr:unit"] = `${originalUnitName} ${returnObject["addr:unit"]}`.trim();
+		returnObject["addr:unit"] = `${unitName} ${returnObject["addr:unit"]}`.trim();
 	}
 
 	return returnObject;
