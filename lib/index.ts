fbd4891160d6c1d4ff11c509cb8c86914a17cd49
Charlie Fish <fishcharlie@me.com>
2021-07-11 17:58:39 -0600

    Exporting UnitParser

--- lib/index.ts
+++ lib/index.ts
@@ -1,9 +1,11 @@
 import * as DirectionAbbreviations from "./abbreviations/direction";
 import AddressParser from "./address/parser";
 import StreetStandardize from "./address/street/standardize";
+import UnitParser from "./address/unit/parser";
 
 export = {
 	DirectionAbbreviations,
 	AddressParser,
-	StreetStandardize
+	StreetStandardize,
+	UnitParser
 };
